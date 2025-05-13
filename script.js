2fbfcbb78d68c4a0149197bc5b27777bb02e82bb
BhawishyaAccio <bhawishya@acciojob.com>
2025-05-14 03:50:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const parseCode = (str) => {
   // your code here
+	let ans=str.split("000");
+let obj={
+  firstName:ans[0],
+  lastName:ans[1],
+  id:ans[2]
+}
+return obj
 };
 
 // Do not change the code below
